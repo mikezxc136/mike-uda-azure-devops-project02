d9b1f4bb2915e8e77f1f2974194c6db11a92a8a4
mikezxc136 <vantuan1362002@gmail.com>
2024-06-09 13:01:09 +0700

    update

--- commands.sh
+++ commands.sh
@@ -5,4 +5,4 @@
 
 az vm create --resource-group Azuredevops --name my-agent-1 --image Ubuntu2204 --size Standard_DS1_v2 --data-disk-sizes-gb 20 --admin-username devopsagent --admin-password DevOpsAgent@123 --nsg-rule SSH --public-ip-sku Standard
 
-az webapp up --name nguyenlc1-project2 --resource-group Azuredevops --runtime PYTHON:3.9 --sku B1
+az webapp up --name mikezxc-project2 --resource-group Azuredevops --runtime PYTHON:3.9 --sku B1
